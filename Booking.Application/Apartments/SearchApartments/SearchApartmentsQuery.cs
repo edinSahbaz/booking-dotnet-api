cad6a4166727cf342b37080eb09ae619a2933dba
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-08-16 14:38:21 +0200

    Search Apartments

---
+++ Booking.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,7 @@
+using Booking.Application.Abstractions.Messaging;
+
+namespace Booking.Application.Apartments.SearchApartments;
+
+public sealed record SearchApartmentsQuery(
+    DateOnly StartDate,
+    DateOnly EndDate) : IQuery<IReadOnlyList<ApartmentResponse>>;
