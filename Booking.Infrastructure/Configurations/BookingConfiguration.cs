5949c2edd3fb5104ee1d2e405c0f6ef05b5509c5
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-08-17 20:34:49 +0200

    Entity Configurations

---
+++ Booking.Infrastructure/Configurations/BookingConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Booking.Domain.Apartments;
+using Booking.Domain.Shared;
+using Booking.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booking.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Domain.Bookings.Booking>
+{
+    public void Configure(EntityTypeBuilder<Domain.Bookings.Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(booking => booking.Id);
+
+        builder.OwnsOne(booking => booking.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.AmenitiesUpCharge, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.Duration);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(booking => booking.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(booking => booking.UserId);
+    }
+}
---
+++ Booking.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Booking.Domain.Apartments;
+using Booking.Domain.Reviews;
+using Booking.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booking.Infrastructure.Configurations;
+
+internal sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+
+        builder.HasKey(review => review.Id);
+
+        builder.Property(review => review.Rating)
+            .HasConversion(rating => rating.Value, value => Rating.Create(value).Value);
+
+        builder.Property(review => review.Comment)
+            .HasMaxLength(200)
+            .HasConversion(comment => comment.Value, value => new Comment(value));
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.ApartmentId);
+
+        builder.HasOne<Booking.Domain.Bookings.Booking>()
+            .WithMany()
+            .HasForeignKey(review => review.BookingId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(review => review.UserId);
+    }
+}
