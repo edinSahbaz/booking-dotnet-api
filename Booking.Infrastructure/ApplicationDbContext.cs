cc0a046560a2199dc2ab1b8e1ddfcab04f72a02a
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-08-17 21:21:16 +0200

    Added event publishing

--- Booking.Infrastructure/ApplicationDbContext.cs
+++ Booking.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 using Booking.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Booking.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options)
+    private readonly IPublisher _publisher;
+    
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,5 +19,35 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+        
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+                
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
