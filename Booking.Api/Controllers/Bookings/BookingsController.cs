bea858076a83e454be2315bb95aa4e142324826e
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-08-17 22:23:03 +0200

    Added Apartment and Booking Controllers

---
+++ Booking.Api/Controllers/Bookings/BookingsController.cs
@@ -0,0 +1,49 @@
+using Booking.Application.Bookings.GetBooking;
+using Booking.Application.Bookings.ReserveBooking;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Api.Controllers.Bookings;
+
+[Route("api/bookings")]
+[ApiController]
+public class BookingsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public BookingsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetBookingQuery(id);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess ? Ok(result.Value) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ReserveBooking(
+        ReserveBookingRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new ReserveBookingCommand(
+            request.ApartmentId,
+            request.UserId,
+            request.StartDate,
+            request.EndDate);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return CreatedAtAction(nameof(GetBooking), new { id = result.Value }, result.Value);
+    }
+}
